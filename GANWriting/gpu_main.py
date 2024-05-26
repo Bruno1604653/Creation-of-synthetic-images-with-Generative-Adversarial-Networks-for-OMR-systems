f896d61c158b84280a00225bae836a94e1e05ec5
Bruno1604653 <133143201+Bruno1604653@users.noreply.github.com>
2024-05-26 17:19:54 +0200

    Add files via upload

---
+++ GANWriting/gpu_main.py
@@ -0,0 +1,218 @@
+import sys
+import os
+import torch
+import glob
+from torch import optim
+import numpy as np
+import time
+import argparse
+from load_data import loadData as load_data_func
+from network_tro import ConTranModel
+from loss_tro import CER
+
+parser = argparse.ArgumentParser(description='seq2seq net', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('start_epoch', type=int, help='load saved weights from which epoch')
+args = parser.parse_args()
+
+gpu = torch.device('cuda')
+
+OOV = True
+NUM_THREAD = 2
+EARLY_STOP_EPOCH = None
+EVAL_EPOCH = 20
+MODEL_SAVE_EPOCH = 200
+show_iter_num = 500
+LABEL_SMOOTH = True
+Bi_GRU = True
+VISUALIZE_TRAIN = True
+
+BATCH_SIZE = 8
+lr_dis = 1 * 1e-4
+lr_gen = 1 * 1e-4
+lr_rec = 1 * 1e-5
+lr_cla = 1 * 1e-5
+
+CurriculumModelID = args.start_epoch
+
+def all_data_loader():
+    directories = ['./data/images', './dataset1', './dataset2', './data/open_omr_raw']  # Asegúrate de actualizar esto con los directorios correctos de tu dataset
+    train_loader = load_data_func(directories, OOV)
+    return train_loader
+
+def sort_batch(batch):
+    train_idx = list()
+    train_img = list()
+    train_img_width = list()
+    train_label = list()
+    img_xts = list()
+    label_xts = list()
+    label_xts_swap = list()
+    for idx, img, img_width, label, img_xt, label_xt, label_xt_swap in batch:
+        train_idx.append(idx)
+        train_img.append(img)
+        train_img_width.append(img_width)
+        train_label.append(label)
+        img_xts.append(img_xt)
+        label_xts.append(label_xt)
+        label_xts_swap.append(label_xt_swap)
+
+    train_idx = np.array(train_idx)
+    train_img = np.array(train_img, dtype='float32')
+    train_img_width = np.array(train_img_width, dtype='int64')
+    train_label = np.array(train_label, dtype='int64')
+    img_xts = np.array(img_xts, dtype='float32')
+    label_xts = np.array(label_xts, dtype='int64')
+    label_xts_swap = np.array(label_xts_swap, dtype='int64')
+
+    train_img = torch.from_numpy(train_img)
+    train_img_width = torch.from_numpy(train_img_width)
+    train_label = torch.from_numpy(train_label)
+    img_xts = torch.from_numpy(img_xts)
+    label_xts = torch.from_numpy(label_xts)
+    label_xts_swap = torch.from_numpy(label_xts_swap)
+
+    return train_idx, train_img, train_img_width, train_label, img_xts, label_xts, label_xts_swap
+
+def train(train_loader, model, dis_opt, gen_opt, rec_opt, cla_opt, epoch):
+    model.train()
+    loss_dis = list()
+    loss_dis_tr = list()
+    loss_cla = list()
+    loss_cla_tr = list()
+    loss_l1 = list()
+    loss_rec = list()
+    loss_rec_tr = list()
+    time_s = time.time()
+    cer_tr = CER()
+    cer_te = CER()
+    cer_te2 = CER()
+    for train_data_list in train_loader:
+        '''rec update'''
+        rec_opt.zero_grad()
+        l_rec_tr = model(train_data_list, epoch, 'rec_update', cer_tr)
+        rec_opt.step()
+
+        '''classifier update'''
+        cla_opt.zero_grad()
+        l_cla_tr = model(train_data_list, epoch, 'cla_update')
+        cla_opt.step()
+
+        '''dis update'''
+        dis_opt.zero_grad()
+        l_dis_tr = model(train_data_list, epoch, 'dis_update')
+        dis_opt.step()
+
+        '''gen update'''
+        gen_opt.zero_grad()
+        l_total, l_dis, l_cla, l_l1, l_rec = model(train_data_list, epoch, 'gen_update', [cer_te, cer_te2])
+        gen_opt.step()
+
+        loss_dis.append(l_dis.cpu().item())
+        loss_dis_tr.append(l_dis_tr.cpu().item())
+        loss_cla.append(l_cla.cpu().item())
+        loss_cla_tr.append(l_cla_tr.cpu().item())
+        loss_l1.append(l_l1.cpu().item())
+        loss_rec.append(l_rec.cpu().item())
+        loss_rec_tr.append(l_rec_tr.cpu().item())
+
+    fl_dis = np.mean(loss_dis)
+    fl_dis_tr = np.mean(loss_dis_tr)
+    fl_cla = np.mean(loss_cla)
+    fl_cla_tr = np.mean(loss_cla_tr)
+    fl_l1 = np.mean(loss_l1)
+    fl_rec = np.mean(loss_rec)
+    fl_rec_tr = np.mean(loss_rec_tr)
+
+    res_cer_tr = cer_tr.fin()
+    res_cer_te = cer_te.fin()
+    res_cer_te2 = cer_te2.fin()
+    print('epo%d <tr>-<gen>: l_dis=%.2f-%.2f, l_cla=%.2f-%.2f, l_rec=%.2f-%.2f, l1=%.2f, cer=%.2f-%.2f-%.2f, time=%.1f' % (epoch, fl_dis_tr, fl_dis, fl_cla_tr, fl_cla, fl_rec_tr, fl_rec, fl_l1, res_cer_tr, res_cer_te, res_cer_te2, time.time()-time_s))
+    return res_cer_te + res_cer_te2
+
+def test(test_loader, epoch, modelFile_o_model):
+    if type(modelFile_o_model) == str:
+        model = ConTranModel(show_iter_num, OOV).to(gpu)
+        print('Loading ' + modelFile_o_model)
+        model.load_state_dict(torch.load(modelFile_o_model)) #load
+    else:
+        model = modelFile_o_model
+    model.eval()
+    loss_dis = list()
+    loss_cla = list()
+    loss_rec = list()
+    time_s = time.time()
+    cer_te = CER()
+    cer_te2 = CER()
+    for test_data_list in test_loader:
+        l_dis, l_cla, l_rec = model(test_data_list, epoch, 'eval', [cer_te, cer_te2])
+
+        loss_dis.append(l_dis.cpu().item())
+        loss_cla.append(l_cla.cpu().item())
+        loss_rec.append(l_rec.cpu().item())
+
+    fl_dis = np.mean(loss_dis)
+    fl_cla = np.mean(loss_cla)
+    fl_rec = np.mean(loss_rec)
+
+    res_cer_te = cer_te.fin()
+    res_cer_te2 = cer_te2.fin()
+    print('EVAL: l_dis=%.3f, l_cla=%.3f, l_rec=%.3f, cer=%.2f-%.2f, time=%.1f' % (fl_dis, fl_cla, fl_rec, res_cer_te, res_cer_te2, time.time()-time_s))
+
+def main(train_loader, test_loader):
+    model = ConTranModel(show_iter_num, OOV).to(gpu)
+
+    if CurriculumModelID > 0:
+        model_file = 'save_weights/contran-' + str(CurriculumModelID) + '.model'
+        print('Loading ' + model_file)
+        model.load_state_dict(torch.load(model_file)) #load
+
+    dis_params = list(model.dis.parameters())
+    gen_params = list(model.gen.parameters())
+    rec_params = list(model.rec.parameters())
+    dis_opt = optim.Adam([p for p in dis_params if p.requires_grad], lr=lr_dis)
+    gen_opt = optim.Adam([p for p in gen_params if p.requires_grad], lr=lr_gen)
+    rec_opt = optim.Adam([p for p in rec_params if p.requires_grad], lr=lr_rec)
+    epochs = 50001
+    min_cer = 1e5
+    min_idx = 0
+    min_count = 0
+
+    for epoch in range(CurriculumModelID, epochs):
+        cer = train(train_loader, model, dis_opt, gen_opt, rec_opt, epoch)
+
+        if epoch % MODEL_SAVE_EPOCH == 0:
+            folder_weights = 'save_weights'
+            if not os.path.exists(folder_weights):
+                os.makedirs(folder_weights)
+            torch.save(model.state_dict(), folder_weights + '/contran-%d.model' % epoch)
+
+        if epoch % EVAL_EPOCH == 0:
+            test(test_loader, epoch, model)
+
+        if EARLY_STOP_EPOCH is not None:
+            if min_cer > cer:
+                min_cer = cer
+                min_idx = epoch
+                min_count = 0
+                rm_old_model(min_idx)
+            else:
+                min_count += 1
+            if min_count >= EARLY_STOP_EPOCH:
+                print('Early stop at %d y el mejor epoch es %d' % (epoch, min_idx))
+                model_url = 'save_weights/contran-' + str(min_idx) + '.model'
+                os.system('mv ' + model_url + ' ' + model_url + '.bak')
+                os.system('rm save_weights/contran-*.model')
+                break
+
+def rm_old_model(index):
+    models = glob.glob('save_weights/*.model')
+    for m in models:
+        epoch = int(m.split('.')[0].split('-')[1])
+        if epoch < index:
+            os.system('rm save_weights/contran-' + str(epoch) + '.model')
+
+if __name__ == '__main__':
+    print(time.ctime())
+    train_loader = all_data_loader()
+    main(train_loader, None)
+    print(time.ctime())
---
+++ GANWriting/modules_tro.py
@@ -0,0 +1,251 @@
+import numpy as np
+import os
+import torch
+from torch import nn
+from blocks import LinearBlock, Conv2dBlock, ResBlocks, ActFirstResBlock
+from vgg_tro_channel3_modi import vgg19_bn
+from recognizer.models.encoder_vgg import Encoder as rec_encoder
+from recognizer.models.decoder import Decoder as rec_decoder
+from recognizer.models.seq2seq import Seq2Seq as rec_seq2seq
+from recognizer.models.attention import locationAttention as rec_attention
+from load_data import IMG_HEIGHT, IMG_WIDTH, vocab_size, index2letter, num_tokens, tokens
+import cv2
+
+device = torch.device('cpu' if not torch.cuda.is_available() else 'cuda')
+
+def normalize(tar):
+    min_val = tar.min()
+    max_val = tar.max()
+    
+    if max_val != min_val:
+        tar = (tar - min_val) / (max_val - min_val)
+    else:
+        tar = np.zeros_like(tar)  # Asigna una matriz de ceros si min y max son iguales
+    
+    tar = tar * 255
+    tar = tar.astype(np.uint8)
+    return tar
+
+
+def fine(label_list):
+    if type(label_list) != type([]):
+        return [label_list]
+    else:
+        return label_list
+
+def write_image(xg, pred_label, gt_img, gt_label, title):
+    folder = 'imgs'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    batch_size = gt_label.shape[0]
+    gt_img = gt_img.cpu().numpy()
+    xg = xg.cpu().numpy()
+    gt_label = gt_label.cpu().numpy()
+    pred_label = torch.topk(pred_label, 1, dim=-1)[1].squeeze(-1)  # b,t,83 -> b,t,1 -> b,t
+    pred_label = pred_label.cpu().numpy()
+
+    outs = list()
+    for i in range(batch_size):
+        gt = gt_img[i].squeeze()
+        tar = xg[i].squeeze()
+        gt = normalize(gt)
+        tar = normalize(tar)
+        gt_text = gt_label[i].tolist()
+        pred_text = pred_label[i].tolist()
+
+        gt_text = fine(gt_text)
+        pred_text = fine(pred_text)
+
+        for j in range(num_tokens):
+            gt_text = list(filter(lambda x: x != j, gt_text))
+            pred_text = list(filter(lambda x: x != j, pred_text))
+
+        gt_text = ''.join([index2letter[c - num_tokens] for c in gt_text])
+        pred_text = ''.join([index2letter[c - num_tokens] for c in pred_text])
+        gt_text_img = np.zeros_like(tar)
+        pred_text_img = np.zeros_like(tar)
+        cv2.putText(gt_text_img, gt_text, (5, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(pred_text_img, pred_text, (5, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        out = np.vstack([gt, gt_text_img, tar, pred_text_img])
+        outs.append(out)
+    final_out = np.hstack(outs)
+    cv2.imwrite(folder + '/' + title + '.png', final_out)
+
+def assign_adain_params(adain_params, model):
+    # assign the adain_params to the AdaIN layers in model
+    for m in model.modules():
+        if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+            mean = adain_params[:, :m.num_features]
+            std = adain_params[:, m.num_features:2*m.num_features]
+            m.bias = mean.contiguous().view(-1)
+            m.weight = std.contiguous().view(-1)
+            if adain_params.size(1) > 2*m.num_features:
+                adain_params = adain_params[:, 2*m.num_features:]
+
+def get_num_adain_params(model):
+    # return the number of AdaIN parameters needed by the model
+    num_adain_params = 0
+    for m in model.modules():
+        if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+            num_adain_params += 2*m.num_features
+    return num_adain_params
+
+class DisModel(nn.Module):
+    def __init__(self):
+        super(DisModel, self).__init__()
+        self.n_layers = 6
+        self.final_size = 1024
+        nf = 16
+        cnn_f = [Conv2dBlock(1, nf, 7, 1, padding=3, pad_type='reflect', norm='none', activation='none')]
+        for i in range(self.n_layers - 1):
+            nf_out = min(nf * 2, 1024)
+            cnn_f += [ActFirstResBlock(nf, nf, norm='none', activation='lrelu')]
+            cnn_f += [ActFirstResBlock(nf, nf_out, norm='none', activation='lrelu')]
+            cnn_f += [nn.ReflectionPad2d(1)]
+            cnn_f += [nn.AvgPool2d(kernel_size=3, stride=2, padding=1)]
+            nf = min(nf * 2, 1024)
+        nf_out = min(nf * 2, 1024)
+        cnn_f += [ActFirstResBlock(nf, nf, norm='none', activation='lrelu')]
+        cnn_f += [ActFirstResBlock(nf, nf_out, norm='none', activation='lrelu')]
+
+        self.cnn_f = nn.Sequential(*cnn_f)
+
+        example_input = torch.randn(1, 1, 128, 128)
+        example_feat = self.cnn_f(example_input)
+        flattened_size = np.prod(example_feat.shape[1:])
+
+        #print(f"Flattened size: {flattened_size}")
+
+        cnn_c = [
+            nn.Flatten(),
+            nn.Linear(flattened_size, self.final_size),
+            nn.LeakyReLU(0.2, inplace=False)
+        ]
+        self.cnn_c = nn.Sequential(*cnn_c)
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def forward(self, x):
+        #(f"x.shape in forward DisModel: {x.shape}")
+        feat = self.cnn_f(x)
+        #print(f"feat.shape after cnn_f: {feat.shape}")
+        out = self.cnn_c(feat)
+        #print(f"out.shape after cnn_c: {out.shape}")
+        return out
+
+
+    def calc_dis_fake_loss(self, input_fake):
+        #print(f"input_fake.shape: {input_fake.shape}")
+        label = torch.zeros(input_fake.shape[0], self.final_size).to(device)
+        resp_fake = self.forward(input_fake)
+        fake_loss = self.bce(resp_fake, label)
+        return fake_loss
+
+    def calc_dis_real_loss(self, input_real):
+        #print(f"input_real.shape: {input_real.shape}")
+        label = torch.ones(input_real.shape[0], self.final_size).to(device)
+        resp_real = self.forward(input_real)
+        real_loss = self.bce(resp_real, label)
+        return real_loss
+
+    def calc_gen_loss(self, input_fake):
+        label = torch.ones(input_fake.shape[0], self.final_size).to(device)
+        resp_fake = self.forward(input_fake)
+        fake_loss = self.bce(resp_fake, label)
+        return fake_loss
+
+
+
+class GenModel_FC(nn.Module):
+    def __init__(self):
+        super(GenModel_FC, self).__init__()
+        self.enc_image = ImageEncoder().to(device)
+        self.dec = Decoder().to(device)
+
+    def decode(self, content):
+        images = self.dec(content)
+        return images
+
+    def forward(self, img):
+        feat_xs = self.enc_image(img)  # Codifica la imagen de entrada
+        #(f"Encoded features shape: {feat_xs.shape}")
+        generated_img = self.decode(feat_xs)  # Decodifica para generar la imagen
+        #print(f"Generated image shape: {generated_img.shape}")
+        return generated_img
+
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        super(ImageEncoder, self).__init__()
+        self.model = vgg19_bn(False)
+        self.output_dim = 512
+
+    def forward(self, x):
+        return self.model(x)
+
+class Decoder(nn.Module):
+    def __init__(self, ups=4, n_res=2, dim=512, out_dim=1, res_norm='adain', activ='relu', pad_type='reflect'):
+        super(Decoder, self).__init__()
+
+        self.model = []
+        self.model += [ResBlocks(n_res, dim, "none", activ, pad_type=pad_type)]
+        for i in range(ups):
+            self.model += [nn.Upsample(scale_factor=2),
+                           Conv2dBlock(dim, dim // 2, 5, 1, 2,
+                                       norm='in',
+                                       activation=activ,
+                                       pad_type=pad_type)]
+            dim //= 2
+        self.model += [Conv2dBlock(dim, out_dim, 7, 1, 3,
+                                   norm='none',
+                                   activation='tanh',
+                                   pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class RecModel(nn.Module):
+    def __init__(self, pretrain=False):
+        super(RecModel, self).__init__()
+        hidden_size_enc = hidden_size_dec = 512
+        embed_size = 60
+        self.enc = rec_encoder(hidden_size_enc, IMG_HEIGHT, IMG_WIDTH, True, None, False).to(device)
+        self.dec = rec_decoder(hidden_size_dec, embed_size, vocab_size, rec_attention, None).to(device)
+        self.seq2seq = rec_seq2seq(self.enc, self.dec, vocab_size).to(device)
+        if pretrain:
+            model_file = 'recognizer/save_weights/seq2seq-72.model_5.79.bak'
+            self.seq2seq.load_state_dict(torch.load(model_file))
+
+    def forward(self, img, label, img_width):
+        self.seq2seq.train()
+        img = torch.cat([img, img, img], dim=1)  # b,1,64,128->b,3,64,128
+        output, attn_weights = self.seq2seq(img, label, img_width, teacher_rate=False, train=False)
+        return output.permute(1, 0, 2)  # t,b,83->b,t,83
+
+
+
+
+class MLP(nn.Module):
+    def __init__(self, in_dim=64, out_dim=4096, dim=256, n_blk=3, norm='none', activ='relu'):
+        super(MLP, self).__init__()
+        self.model = []
+        self.model += [LinearBlock(in_dim, dim, norm=norm, activation=activ)]
+        for i in range(n_blk - 2):
+            self.model += [LinearBlock(dim, dim, norm=norm, activation=activ)]
+        self.model += [LinearBlock(dim, out_dim,
+                                   norm='none', activation='none')]
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
+if __name__ == "__main__":
+    # Prueba del generador
+    gen_model = GenModel_FC()
+    sample_img = torch.randn(128, 1, 128, 128)  # Ejemplo de entrada
+    generated_img = gen_model(sample_img)
+    #print(f"Generated image shape: {generated_img.shape}")
+
+    # Prueba del discriminador
+    dis_model = DisModel()
+    output = dis_model(generated_img)
+    #print(f"Output shape: {output.shape}")
