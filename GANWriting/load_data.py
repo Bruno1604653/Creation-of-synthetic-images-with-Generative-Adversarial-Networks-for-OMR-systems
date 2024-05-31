7d7d8f4fc2a70ef14044f5b566acb5e0bdeab3c6
Garqke <brunofajardocortes@gmail.com>
2024-05-31 23:43:05 +0200

    .

--- GANWriting/load_data.py
+++ GANWriting/load_data.py
@@ -5,8 +5,25 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
-MUSICAL_SYMBOLS = ['1-8-time', '12-8-time', '2-4-time', '2-8-time', '3-4-time', '3-8-time', '4-2-time', '4-4-time', '4-8-time', '5-4-time', '5-8-time', '6-4-time', '6-8-time', '7-4-time', '8-8-time', '9-8-time', 'accent', 'barline', 'bass', 'beam', 'breve', 'c-clef', 'chord', 'common-time','crotchet', 'cut-time', 'demisemiquaver_line', 'dot', 'double-whole-rest', 'eighth-grace-note', 'eighth-note', 'eighth-rest', 'f-clef', 'fermata','flat', 'g-clef', 'glissando', 'half-note', 'marcato', 'minim', 'mordent', 'multiple-eighth-notes', 'multiple-half-notes', 'multiple-quarter-notes', 'multiple-sixteenth-notes', 'natural', 'other', 'quarter-note', 'quarter-rest', 'quaver_br', 'quaver_line', 'quaver_tr', 'repeat-measure', 'segno', 'semibreve', 'semiquaver_br', 'semiquaver_line', 'semiquaver_tr', 'sharp', 'sixteenth-note', 'sixteenth-rest', 'sixty-four-note', 'sixty-four-rest', 'staccatissimo', 'stopped', 'tenuto', 'test', 'thirty-two-note', 'thirty-two-rest', 'tie-slur', 'training', 'treble', 'trill', 'trill-wobble', 'tuplet', 'turn', 'v2.0', 'validation', 'volta', 'whole-half-rest', 'whole-note']
 
+file_path = './all_images.txt'
+
+def load_musical_symbols(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"El archivo {file_path} no existe")
+
+    musical_symbols = set()
+    with open(file_path, 'r') as file:
+        for line in file:
+            symbol = line.strip().split('\t')[0]
+            musical_symbols.add(symbol.lower())
+
+    return sorted(musical_symbols)
+
+# Crear la lista de símbolos musicales
+MUSICAL_SYMBOLS = load_musical_symbols(file_path)
+
+# Crear el diccionario de símbolos musicales
 MUSICAL_SYMBOLS_DICT = {symbol: idx for idx, symbol in enumerate(MUSICAL_SYMBOLS)}
 
 IMG_HEIGHT = 128
@@ -20,6 +37,7 @@ tokens = {
     'UNK_TOKEN': 3
 }
 
+# Agregar los símbolos musicales a los tokens
 for i, symbol in enumerate(MUSICAL_SYMBOLS):
     tokens[symbol] = i + 4
 
@@ -27,13 +45,16 @@ index2letter = {v: k for k, v in tokens.items()}
 vocab_size = len(tokens)
 num_tokens = 4
 NUM_CHANNEL = 3
+
 print(f"vocab_size: {vocab_size}")
+
 class MusicSymbolDataset(Dataset):
     def __init__(self, data_dirs, transform=None):
         global tokens
         self.data_dirs = data_dirs
         self.transform = transform or transforms.Compose([
             transforms.Resize((IMG_HEIGHT, IMG_WIDTH)),
+            transforms.Grayscale(num_output_channels=1),
             transforms.ToTensor()
         ])
         self.data = []
@@ -56,15 +77,12 @@ class MusicSymbolDataset(Dataset):
                     for img_file in os.listdir(symbol_dir):
                         if img_file.lower().endswith('.png'):
                             png_count += 1
-                            label = tokens[symbol_lower]
-                            if label >= vocab_size:
-                                print(f"Warning: Label {label} for {symbol_lower} exceeds vocab size {vocab_size}")
-                                continue
-                            self.data.append((os.path.join(symbol_dir, img_file), label))
+                            self.data.append((os.path.join(symbol_dir, img_file), tokens[symbol_lower]))
                             print(f"Añadido: {os.path.join(symbol_dir, img_file)}")
-                    print(f"Archivos .png encontrados en {symbol_dir}: {png_count}")
+                    #print(f"Archivos .png encontrados en {symbol_dir}: {png_count}")
                 else:
-                    print(f"Directorio no encontrado para símbolo: {symbol_dir}")
+                    pass
+                    #print(f"Directorio no encontrado para símbolo: {symbol_dir}")
         print(f"Total de imágenes encontradas: {len(self.data)}")
         print(f"Clases encontradas: {self.classes}")
 
@@ -73,10 +91,10 @@ class MusicSymbolDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path, label = self.data[idx]
-        image = Image.open(img_path).convert('RGB')  # Convertir a RGB
+        image = Image.open(img_path).convert('L')
         image = self.transform(image)
+        #print(f"Loaded image shape: {image.shape}")  # Add this line to print the shape of each loaded image
         return image, label
-
 
 def loadData(oov, directories=None, batch_size=128, num_workers=0):
     if directories is None:
